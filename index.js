cb9829640bd3ee55cb5eef968a85d7a693e58c8e
iknowmagic <ilan@iknowmagic.net>
2022-02-19 17:35:48 -0800

    middlewares - read-only

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./data.json");
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./data.json')
+const middlewares = jsonServer.defaults({ readOnly: true })
 
-server.use(middlewares);
-server.use(router);
+server.use(middlewares)
+server.use(router)
 server.listen(process.env.PORT || 3001, () => {
-  console.log("JSON Server is running");
-});
+  console.log('JSON Server is running')
+})
