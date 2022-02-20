f148ab44efe4231b424010ac40fe0b9343952be9
iknowmagic <ilan@iknowmagic.net>
2022-02-19 17:36:54 -0800

    middlewares - read/write

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./data.json')
-const middlewares = jsonServer.defaults({ readOnly: true })
+const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
